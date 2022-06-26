081ca89b9a0f1b1506a80cef233c923b7c0e62b7
Dmitryi <dimon_smirnov81@mail.ru>
2022-06-26 19:49:46 +0300

    Complete Task 3 DZ 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,3 +3,14 @@
 //6 -> да
 //7 -> да
 //1 -> нет
+
+Console.Write("Введи число дня недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if(x < 6)
+{
+    System.Console.WriteLine("Нет, это не выходной день");
+}
+else
+{
+    System.Console.WriteLine("Ура, это выходной день");
+}
